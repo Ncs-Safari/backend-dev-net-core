94f6e33780e0dede1f41d33edd6508aa055c93c1
safari05 <safari.erie@gmail.com>
2024-09-09 21:12:58 +0700

    Initialize Controller auth

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dev_backend.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
